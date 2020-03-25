68ef974ef7ba7617685a6d65f07a61db18ab61c9
alebanegev <31402247+alebanegev@users.noreply.github.com>
2020-03-25 21:52:02 +0200

    Add files via upload

--- data_structures/reading_two_list.c
+++ data_structures/reading_two_list.c
@@ -1,2 +1,29 @@
+#include "reading_two_list.h"
 
+void add_second_line_reading(ReadingTwoList **ptl, ParsedCommand *ppc, BitsCommand *pbc, int row_num) 
+{
+	ReadingTwoList *pl, *node, *runner;
+    if (!*ptl)
+    {
+        /**First allocation**/
+        pl = (ReadingTwoList *) malloc(sizeof(ReadingTwoList));
+        pl->row_num = row_num;
+        pl->ppc = ppc;
+        pl->pbc = pbc;
+        pl->next = NULL;
+        *ptl = pl;
+    } 
+    else
+    {
+        runner = *ptl;
+        while (runner->next)runner = runner->next;
+        node = (ReadingTwoList *) malloc(sizeof(ReadingTwoList));
+        node->row_num = row_num;
+        pl->ppc = ppc;
+        pl->pbc = pbc;
+        node->next = NULL;
+        runner->next = node;
+    }
 
+    printf("hey\n");
+}
--- data_structures/reading_two_list.h
+++ data_structures/reading_two_list.h
@@ -10,4 +10,7 @@ typedef struct ReadingTwoList {
     int row_num;
     struct symbols_list *next;
 } ReadingTwoList;
-#endif //FINAL_PROJECT_READING_TWO_LIST_H
+
+void add_second_line_reading(ReadingTwoList **ptl, ParsedCommand *ppc, BitsCommand *pbc, int row_num);
+
+#endif /*FINAL_PROJECT_READING_TWO_LIST_H*/
