76a781ec35733c39fe35bc8c787ccbd02e255d02
alebanegev <31402247+alebanegev@users.noreply.github.com>
2020-03-27 12:59:26 +0300

    Update instant_addressing.h

--- addressing/instant_addressing.h
+++ addressing/instant_addressing.h
@@ -1,3 +1,6 @@
+#ifndef FINAL_PROJECT_INSTANT_ADDRESSING_H
+#define FINAL_PROJECT_INSTANT_ADDRESSING_H
+
 #include <stdio.h>
 
 #include "../consts.h"
@@ -9,3 +12,5 @@
 #include "../data_structures/reading_two_list.h"
 
 void run_instant_addressing(InstructionCount *ic, ParsedCommand *, BitsCommand *, int, ReadingTwoList **);
+
+#endif /*FINAL_PROJECT_INSTANT_ADDRESSING_H*/
