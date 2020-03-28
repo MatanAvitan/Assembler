ed5b45e678c44adc61620c59e886104af906b94f
alebanegev <31402247+alebanegev@users.noreply.github.com>
2020-03-28 23:36:25 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 #include "utils.h"
 #include "file_handler.h"
 #include "data_structures/instruction_counter.h"
+#include "validator.h"
 
 
 int main() {
@@ -120,6 +121,7 @@ int main() {
                 is_entry_or_extern = 0;
                 is_labeled_command = 1;
                 ic->row = backup_row;
+                first_round = valid_label(ppc->prefix);
                 add_symbol(&sl, ppc->prefix, ic, NULL, NULL, NULL,
                            is_entry_or_extern, is_labeled_command);
             }
