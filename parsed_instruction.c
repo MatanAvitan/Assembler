c74f0583767d034441c2b6e6341fc6b4b780d334
Matan Avitan <matan13av@gmail.com>
2020-03-28 22:56:51 +0300

    Fully support in string command with quotas

--- parsed_instruction.c
+++ parsed_instruction.c
@@ -14,18 +14,26 @@ int get_instruction_type(char *instruction) {
 }
 
 void parse_instruction(char *command, ParsedInstruction *ppi) {
-    char seps[] = " ,\t\n";
+    char seps[] = " ,\t\n", quata_sep = '"';
     char label[MAX_INSTRUCTION_LEN];
     char *token;
     char instruction[MAX_INSTRUCTION_LEN];
     LinkedList *node, *runner;
     ppi->members_num = 0;
     token = strtok(command, seps);
+    int quata_started = 0;
     if (token[0] == '.') {
         strcpy(instruction, token + 1);
         ppi->instruction_type = get_instruction_type(instruction);
     }
     token = strtok(NULL, seps);
+    if (token[0] == quata_sep) {
+        token++; /**Come over the quata**/
+        quata_started = 1;
+    } else {
+        printf("There is no starting quata");
+    }
+
     if (strcmp(instruction, DATA) == 0) {
         if (token) {
             node = (LinkedList *) malloc(sizeof(LinkedList));
@@ -55,7 +63,10 @@ void parse_instruction(char *command, ParsedInstruction *ppi) {
         }
         token++;
         while (*token) {
-            if (*token == "\n")break;
+            if (*token == quata_sep && quata_started) {
+                quata_started = 0;
+                break;
+            }
             node = (LinkedList *) malloc(sizeof(LinkedList));
             node->val = *token;/**Assign each char as int(ascii representation)**/
             node->next = NULL;
@@ -65,11 +76,23 @@ void parse_instruction(char *command, ParsedInstruction *ppi) {
             ppi->members_num += 1;
             token++;
         }
+        if (quata_started == 1) {
+            printf("There is starting quata and no ending quata");
+        } else {
+            /**We go a valid string**/
+            /**Let's add a NULL termination char**/
+            node = (LinkedList *) malloc(sizeof(LinkedList));
+            node->val = NULL;/**Assign each char as int(ascii representation)**/
+            node->next = NULL;
+            runner = &ppi->list;
+            while (runner->next) runner = runner->next;
+            runner->next = node;
+            ppi->members_num += 1;
+        }
     } else if ((strcmp(instruction, ENTRY) == 0) || (strcmp(instruction, EXTERN) == 0)) {
         if (*token) {
             node = (LinkedList *) malloc(sizeof(LinkedList));
 
-            /*NEW*/
             strcpy(ppi->label, token);
 
             strcpy(node->val_for_labels, token); /**Assign each char as int(ascii representation)**/
@@ -77,7 +100,7 @@ void parse_instruction(char *command, ParsedInstruction *ppi) {
             ppi->list = *node;
             ppi->members_num += 1;
 
-            
+
         } else {
             /*todo: there is no label after entry*/
         }
